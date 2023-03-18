73d919da303efe71ff13f416a3ea665560b6918f
singhluvkush286 <singhlAvkush286@gmail.com>
2023-03-19 00:37:45 +0530

    Guessing Game

---
+++ project/Guess_Game/Guesser.java
@@ -0,0 +1,12 @@
+package Guesser_Game;
+import java.util.*;
+import java.lang.*;
+
+public class Guesser {
+    public int guessNumber()
+    {
+       int  num;
+       num =(int)(Math.random()*10);
+       return num;
+    }
+}
